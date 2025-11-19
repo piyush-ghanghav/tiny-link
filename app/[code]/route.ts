fec2171a900659383ba342e5172c09d6304162b9
piyush-ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2025-11-19 11:20:36 +0530

    feat: redirect handler with click tracking complete

---
+++ app/[code]/route.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+
+export async function GET(
+    request: Request,
+    { params }: { params:  { code: string } }
+){
+    try {
+        const { code } = await params;
+
+        const link = await prisma.link.findFirst({
+            where: {
+                code: code,
+                deleted_at: null,
+            },
+        });
+
+        if(!link) {
+            return NextResponse.json(
+                { error: "Link not found" },
+                { status: 404 }
+            );
+        }
+
+        await prisma.link.update({
+            where: { code: code },
+            data: { clicks: { increment: 1 }, last_clicked_at: new Date() },
+        });
+
+        return NextResponse.redirect(link.target_url, { status: 302 });
+
+    } catch (error) {
+        console.error("Error redirecting:", error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}

59abf426d04141bc6e6e58f345f11bd4313f0f72
piyush-ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2025-11-20 18:15:04 +0530

    fix: prisma client initialisation error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/client/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
